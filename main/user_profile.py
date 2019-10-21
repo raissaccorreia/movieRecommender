da41361b98f2af95c28ea97eeab6459db9e57307
Raissa Correia <raisssa.correia@gmail.com>
2019-10-21 08:43:05 -0300

    user profiling almost there

--- main/user_profile.py
+++ main/user_profile.py
@@ -1,46 +1,80 @@
 import pandas as pd
 import numpy as np
 
-#linha,trash,movieId,tagId,relevance,label
-#movie_profile = pd.read_csv("../ML_Dataset/ml-latest/genome-tags.csv")
+# * linha,trash,movieId,tagId,relevance,label
+# movie_profile = pd.read_csv("../ML_Dataset/ml-latest/genome-tags.csv")
 
-#userId,movieId,rating,timestamp
+# * userId,movieId,rating,timestamp
 ratings = pd.read_csv("../ML_Dataset/ml-latest-small/ratings.csv")
-#movieId,title,genres
+# * movieId,title,genres
 movie_genres = pd.read_csv("../ML_Dataset/ml-latest-small/movies.csv")
 
-genre_dict={'Action':0,'Adventure':1,'Animation':2,"Children":3,"Comedy":4,"Crime":5,"Documentary":6,"Drama":7,"Fantasy":8,
-"Film-Noir":9,"Horror":10,"Musical":11, "Mystery":12,"Romance":13,"Sci-Fi":14, "Thriller":15,"War":16,"Western":17,"IMAX":18,
-"(no genres listed)":19}
+genre_dict = {
+    "Action": 0,
+    "Adventure": 1,
+    "Animation": 2,
+    "Children": 3,
+    "Comedy": 4,
+    "Crime": 5,
+    "Documentary": 6,
+    "Drama": 7,
+    "Fantasy": 8,
+    "Film-Noir": 9,
+    "Horror": 10,
+    "Musical": 11,
+    "Mystery": 12,
+    "Romance": 13,
+    "Sci-Fi": 14,
+    "Thriller": 15,
+    "War": 16,
+    "Western": 17,
+    "IMAX": 18,
+    "(no genres listed)": 19,
+}
 
 num_movies = 193610
-genre_movie_matrix = np.zeros(shape=(num_movies,len(genre_dict)),dtype=int)
+genre_movie_matrix = np.zeros(shape=(num_movies, len(genre_dict)), dtype=int)
 
 for i in range(movie_genres.iloc[:, 2].values.size):
     genre_list = movie_genres.iloc[i, 2]
     genre_array = genre_list.split("|")
-    for j in range(len(genre_array)):        
+    for j in range(len(genre_array)):
         index_y = genre_dict[genre_array[j]]
-        genre_movie_matrix[movie_genres.iloc[i, 0],index_y] = 1
+        genre_movie_matrix[movie_genres.iloc[i, 0], index_y] = 1
 
-df = pd.DataFrame(genre_movie_matrix)
-print(df)
-#ate aqui tudo perfeitinho <3
+genre_movie_df = pd.DataFrame(genre_movie_matrix)
 
-#CRIAR DATAFRAME(NUM_USUARIO X GENERO - FLOAT CORRELATION)
-#calcular n dado que esta por ordem de usuario
-#criar array tamanho(1xnum_genero)
-#calcular array iterando genero_assoc*rating +=
-#divide array todo por n e append no novo dataset
+user_profiles = pd.DataFrame(columns=genre_dict, dtype=float)
+
+num_ratings_user = 0
+for i in range(ratings.iloc[:, 0].values.size):
+    # actual,previous and next user to make the decisions
+    user_id = ratings.iloc[i, 0]
+    prev_user = ratings.iloc[i - 1, 0]
+    next_user = ratings.iloc[i + 1, 0]
+    # if actual is not the previous
+    if user_id > prev_user:
+        relation_gender_user = np.zeros(len(genre_dict), dtype=float)
+        num_ratings_user = 0
+    # the same user
+    movie_id = ratings.iloc[i, 1]
+    relation_gender_user += genre_movie_df.loc[movie_id] * ratings.iloc[i, 2]
+    num_ratings_user += 1
+    # if its the last movie of this user
+    if user_id < next_user:
+        relation_gender_user = relation_gender_user * 1 / (num_ratings_user)
+        pd.concat([user_profiles(relation_gender_user)], ignore_index=True)
+
+user_profiles.to_csv("../ML_Dataset/ml-latest-small/user_profile.csv")
 
 ############ parte futura ########
 
 # Criar Matriz usuario-genero-relevance
 # Produto Usuario->MovieId->Genero->Relavance*Rating
 
-#Criar relacao usuario-tag(1128Dim)
-    # Criar Matriz usuario-tag-relevance
-    # Produto Usuario->MovieId->TagId->Relavance*Rating
+# Criar relacao usuario-tag(1128Dim)
+# Criar Matriz usuario-tag-relevance
+# Produto Usuario->MovieId->TagId->Relavance*Rating
 
-#Criar cluster de usuarios
+# Criar cluster de usuarios
 
