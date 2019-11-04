98a0b1737a258d2ee4cfb659944525906e2da3eb
Raissa Correia <raisssa.correia@gmail.com>
2019-11-04 17:58:54 -0300

    pca with plot

--- main/dataPrep/pca.py
+++ main/dataPrep/pca.py
@@ -2,10 +2,37 @@
 # https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
 # https://scikit-learn.org/stable/modules/decomposition.html#pca
 
+# plot
+# https://scikit-learn.org/stable/auto_examples/decomposition/plot_pca_iris.html#sphx-glr-auto-examples-decomposition-plot-pca-iris-py
+
+
 # PCA in movie_profiles and user_profiles
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+genre_dict = {
+    "Action": 0,
+    "Adventure": 1,
+    "Animation": 2,
+    "Children": 3,
+    "Comedy": 4,
+    "Crime": 5,
+    "Documentary": 6,
+    "Drama": 7,
+    "Fantasy": 8,
+    "Film-Noir": 9,
+    "Horror": 10,
+    "Musical": 11,
+    "Mystery": 12,
+    "Romance": 13,
+    "Sci-Fi": 14,
+    "Thriller": 15,
+    "War": 16,
+    "Western": 17,
+    "IMAX": 18,
+}
 
 # * userId,movieId,rating,timestamp
 user_prof = pd.read_csv("../../ML_Dataset/ml-latest-small/user_profile.csv")
@@ -29,7 +56,17 @@ pca = PCA(
 print("MOVIE PROFILES INFO\n")
 print("PCA FIT: ", pca.fit(movie_prof))
 print("PCA FIT Tranform", pca.fit_transform(movie_prof))
+
 print("Expained Variance Ratio: ", pca.explained_variance_ratio_)
+relevance_features = pca.explained_variance_ratio_
+index = np.arange(len(relevance_features))
+plt.bar(index, relevance_features, log=True)
+plt.xlabel("Genre Features", fontsize=5)
+plt.ylabel("Explained Variance Ratio", fontsize=5)
+plt.xticks(index, genre_dict.keys(), fontsize=5, rotation=30)
+plt.title("Explained Variance Ratio for Movie Profiles")
+plt.show()
+
 print("Singular Values: ", pca.singular_values_)
 print("Get Covariance: ", pca.get_covariance())
 print("Get Params: ", pca.get_params(deep=True))
@@ -43,7 +80,17 @@ print("\n")
 print("USER PROFILES INFO\n")
 print("PCA FIT: ", pca.fit(user_prof))
 print("PCA FIT Tranform", pca.fit_transform(user_prof))
+
 print("Expained Variance Ratio: ", pca.explained_variance_ratio_)
+relevance_features = pca.explained_variance_ratio_
+index = np.arange(len(relevance_features))
+plt.bar(index, relevance_features, log=True)
+plt.xlabel("Genre Features", fontsize=5)
+plt.ylabel("Explained Variance Ratio", fontsize=5)
+plt.xticks(index, genre_dict.keys(), fontsize=5, rotation=30)
+plt.title("Explained Variance Ratio for User Profiles")
+plt.show()
+
 print("Singular Values: ", pca.singular_values_)
 print("Get Covariance: ", pca.get_covariance())
 print("Get Params: ", pca.get_params(deep=True))
