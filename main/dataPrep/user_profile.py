21b6616ddc0c6d9eaf68407479b0be912ecb9535
Raissa Correia <raisssa.correia@gmail.com>
2019-11-03 23:32:33 -0300

    user and movie profile correted after long tail plot

--- main/dataPrep/user_profile.py
+++ main/dataPrep/user_profile.py
@@ -3,9 +3,9 @@ import numpy as np
 from tqdm import tqdm
 
 # * userId,movieId,rating,timestamp
-ratings = pd.read_csv("../../ML_Dataset/ml-latest-small/ratings.csv")
+ratings = pd.read_csv("../../ML_Dataset/ml-latest-small/relevant_ratings.csv")
 # * movieId,title,genres
-movie_genres = pd.read_csv("../../ML_Dataset/ml-latest-small/movies.csv")
+movie_genres = pd.read_csv("../../ML_Dataset/ml-latest-small/relevant_movies.csv")
 
 genre_dict = {
     "Action": 0,
@@ -30,50 +30,60 @@ genre_dict = {
     "(no genres listed)": 19,
 }
 
-num_movies = 193610
+num_movies = 2000  # * due to long tail crop
 genre_movie_matrix = np.zeros(shape=(num_movies, len(genre_dict)), dtype=int)
 
-for i in tqdm(range(movie_genres.iloc[:, 2].values.size)):
-    genre_list = movie_genres.iloc[i, 2]
+for i in tqdm(range(num_movies)):
+    genre_list = movie_genres.iloc[i, 4]  # * genres list action|adventure
     genre_array = genre_list.split("|")
     for j in range(len(genre_array)):
         index_y = genre_dict[genre_array[j]]
         genre_movie_matrix[movie_genres.iloc[i, 0], index_y] = 1
 
 genre_movie_df = pd.DataFrame(genre_movie_matrix)
+movie_ids = pd.DataFrame(movie_genres.iloc[:, 2])
 genre_movie_df.to_csv("../../ML_Dataset/ml-latest-small/movie_profiles.csv")
-user_profiles = pd.DataFrame(columns=genre_dict, dtype=float)
 
+movie_id_list = movie_ids["movieId"].tolist()
+movies_dict = {movie_id_list[i]: i for i in range(0, len(movie_id_list))}
+
+user_profiles = pd.DataFrame(columns=genre_dict, dtype=float)
 num_ratings_user = 0
 first_user = False
 last_user = False
 
-for i in tqdm(range(ratings.iloc[:, 0].values.size)):
-    # actual,previous, next, first and last to make the decisions
+num_ratings = 89721
+
+for i in tqdm(range(num_ratings)):
+    # * actual,previous, next, first and last to make the decisions
     if i == 0:
         first_user = True
-    if i == (ratings.iloc[:, 0].values.size) - 1:
+    if i == (num_ratings) - 1:
         last_user = True
     if first_user:
-        user_id = ratings.iloc[i, 0]
-        next_user = ratings.iloc[i + 1, 0]
+        user_id = ratings.iloc[i, 1]
+        next_user = ratings.iloc[i + 1, 1]
         relation_gender_user = np.zeros(len(genre_dict), dtype=float)
         num_ratings_user = 0
     elif last_user:
-        prev_user = ratings.iloc[i - 1, 0]
-        user_id = ratings.iloc[i, 0]
+        prev_user = ratings.iloc[i - 1, 1]
+        user_id = ratings.iloc[i, 1]
         next_user = user_id + 1
     else:
-        prev_user = ratings.iloc[i - 1, 0]
-        user_id = ratings.iloc[i, 0]
-        next_user = ratings.iloc[i + 1, 0]
+        prev_user = ratings.iloc[i - 1, 1]
+        user_id = ratings.iloc[i, 1]
+        next_user = ratings.iloc[i + 1, 1]
         if user_id > prev_user:
             relation_gender_user = np.zeros(len(genre_dict), dtype=float)
             num_ratings_user = 0
     # the same user
-    movie_id = ratings.iloc[i, 1]
-    relation_gender_user += genre_movie_df.loc[movie_id] * ratings.iloc[i, 2]
-    num_ratings_user += 1
+    movie_id = ratings.iloc[i, 2]
+    try:
+        movie_index = movies_dict[movie_id]
+        relation_gender_user += genre_movie_df.loc[movie_index] * ratings.iloc[i, 3]
+        num_ratings_user += 1
+    except:
+        pass
     # if its the last movie of this user
     if user_id < next_user:
         relation_gender_user = relation_gender_user * (1 / (num_ratings_user))
